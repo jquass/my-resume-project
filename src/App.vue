7ce52368c58e8ebaa0440d0a5107a20b274c3f2c
Jon Quass <quassj@gmail.com>
2022-04-15 10:04:33 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/js/dist/collapse'
 
 export default {
   name: 'App',
@@ -14,13 +10,7 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <HelloWorld />
+</template>
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,152 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <h2>Hello, World!</h2>
+
+  <div>
+    <h5>{{ helloMsg }}</h5>
   </div>
+
+  <Transition name="input-transition" @after-leave="showButton">
+    <div v-if="!inputDisabled">
+      <div>
+        <input @input="updateHelloMsgDebounce" v-model="helloInput" placeholder="Your Name" type="text" id="name"
+               name="name"
+               maxlength="20" :disabled="inputDisabled" autocomplete="off">
+      </div>
+    </div>
+  </Transition>
+  <Transition name="button-transition" @after-leave="showInput">
+    <div v-if="!buttonDisabled">
+      <button :disabled="buttonDisabled" @click="hideButton">Did I Get Your Name Wrong?</button>
+      <div v-if="censored">
+        <p>I had to censor part of your name. For more information on what words are censored, see <a
+            href="https://www.purgomalum.com/index.html" target="_blank">PurgoMalum</a></p>
+      </div>
+      <div v-if="cat">
+        <img id="cat" :src="image.url">
+        <h5>Powered by <a href="https://thecatapi.com" target="_blank">The Cat API </a></h5>
+        <button @click="updateCat">New Cat Please</button>
+        <p>{{image.count}} Cats Fetched That I Can Remember</p>
+      </div>
+    </div>
+  </Transition>
 </template>
 
 <script>
+
+import debounce from "debounce";
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      defaultHelloMsg: "Let me know your name for a personalized message!",
+      helloMsg: null,
+      helloInput: null,
+      updateHelloMsgDebounce: null,
+      inputDisabled: false,
+      buttonDisabled: true,
+      censored: false,
+      cat: false,
+      image: null
+    }
+  },
+  methods: {
+    async processHelloMsg() {
+      this.hideInput();
+      await Promise.all([this.updateHelloMessage(), this.checkForCat()]);
+    },
+    async updateHelloMessage() {
+      let params = {}
+      if (this.helloInput && this.helloInput.length > 0) {
+        params.name = this.helloInput
+      }
+      let response = await axios.get('http://localhost:9000/hello-world', {
+        params: params,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      });
+      let data = response['data'];
+      console.log(data);
+      this.helloMsg = data["message"] + " I have made " + data["count"] + " greetings that I can remember!";
+      this.censored = Boolean(data["censored"]);
+    },
+    async checkForCat() {
+      this.cat = this.helloInput.toLowerCase() === 'cat';
+      if (this.cat) {
+       await this.updateCat();
+      }
+    },
+    async updateCat() {
+      let response = await axios.get('http://localhost:9000/the-cat-api', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      });
+      let data = response['data'];
+      console.log(data);
+      this.image = data;
+    },
+    showButton() {
+      this.buttonDisabled = false;
+    },
+    hideButton() {
+      this.buttonDisabled = true;
+    },
+    showInput() {
+      this.initializeHelloMsg();
+      this.helloInput = null;
+      this.inputDisabled = false;
+    },
+    hideInput() {
+      this.inputDisabled = true;
+    },
+    initializeHelloMsg() {
+      this.helloMsg = this.defaultHelloMsg;
+    }
+  },
+  created() {
+    this.updateHelloMsgDebounce = debounce(this.processHelloMsg, 800);
+    this.initializeHelloMsg();
+
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h2 {
+  margin: 40px 10px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+div {
+  margin: 20px 20px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#cat {
+  max-width: 75%;
 }
-a {
-  color: #42b983;
+
+.input-transition-enter-active,
+.input-transition-leave-active {
+  transition: opacity 0.5s ease;
 }
+
+.input-transition-enter-from,
+.input-transition-leave-to {
+  opacity: 0;
+}
+
+.button-transition-enter-active,
+.button-transition-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.button-transition-enter-from,
+.button-transition-leave-to {
+  opacity: 0;
+}
+
 </style>
